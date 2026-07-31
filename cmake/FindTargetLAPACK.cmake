dde7232cdd23b16132edb315ed11827c48514b78
Connor Briggs <cgbriggs99@gmail.com>
2026-07-31 11:10:09 -0400

    Fix dependency versions

--- cmake/FindTargetLAPACK.cmake
+++ cmake/FindTargetLAPACK.cmake
@@ -158,6 +158,12 @@ get_property(
   TARGET tgt::lapack
   PROPERTY INT_INTERFACE
 )
+
+# Set up libatomic if needed.
+if(_ven STREQUAL "MKL")
+	target_link_libraries(tgt::lapack INTERFACE $<$<PLATFORM_ID:Linux>:atomic>)
+endif()
+
 set(${PN}_MESSAGE "Found LAPACK ${_ven}w/${_int}: ${_ill}")
 if((TARGET tgt::blas) AND (TARGET tgt::lapk))
   get_property(
@@ -170,6 +176,12 @@ if((TARGET tgt::blas) AND (TARGET tgt::lapk))
     TARGET tgt::lapk
     PROPERTY INTERFACE_LINK_LIBRARIES
   )
+  
+  # Set up libatomic if needed.
+  if(_ven STREQUAL "MKL")
+	target_link_libraries(tgt::blas INTERFACE $<$<PLATFORM_ID:Linux>:atomic>)
+	target_link_libraries(tgt::lapk INTERFACE $<$<PLATFORM_ID:Linux>:atomic>)
+  endif()
   set(${PN}_MESSAGE "Found LAPACK ${_ven}w/${_int}: ${_illl};${_illb}")
 endif()
 
