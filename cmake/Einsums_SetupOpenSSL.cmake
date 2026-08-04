b24fa6ebacc936feee7b913dc147bcbca7a542f6
Connor Briggs <cgbriggs99@gmail.com>
2026-08-04 14:49:20 -0400

    Add more diagnostics to the failing test.

--- cmake/Einsums_SetupOpenSSL.cmake
+++ cmake/Einsums_SetupOpenSSL.cmake
@@ -6,4 +6,12 @@
 # The rand_s function needs to be cryptographically secure. Everyone has openssl since it's a dependency
 # of HDF5.
 
-find_package(OpenSSL COMPONENTS Crypto)
+message("-- Finding OpenSSL::Crypto")
+find_package(OpenSSL COMPONENTS Crypto REQUIRED)
+
+if(OpenSSL_FOUND)
+  message("-- Found OpenSSL version ${OpenSSL_VERSION}.")
+  message("-- OpenSSL libraries: ${OPENSSL_CRYPTO_LIBRARIES}")
+else()
+  message("-- Could not find OpenSSL!")
+endif()
