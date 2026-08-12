761d147db7b38847a2c311ddc39984c4faab98a7
Connor Briggs <cgbriggs99@gmail.com>
2026-08-12 17:55:19 -0400

    Trying again.

--- testing/testing/src/main.cpp
+++ testing/testing/src/main.cpp
@@ -71,11 +71,11 @@ int main(int argc, char **argv) {
 
 #    ifdef EINSUMS_DEBUG
     _CrtSetReportMode(_CRT_ASSERT, _CRTDBG_MODE_FILE);
-    _CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_FILE);
-    _CrtSetReportMode(_CRT_ERROR, _CRTDBG_MODE_FILE);
+//    _CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_FILE);
+//    _CrtSetReportMode(_CRT_ERROR, _CRTDBG_MODE_FILE);
     _CrtSetReportFile(_CRT_ASSERT, _CRTDBG_FILE_STDERR);
-    _CrtSetReportFile(_CRT_WARN, _CRTDBG_FILE_STDERR);
-    _CrtSetReportFile(_CRT_ERROR, _CRTDBG_FILE_STDERR);
+//    _CrtSetReportFile(_CRT_WARN, _CRTDBG_FILE_STDERR);
+//    _CrtSetReportFile(_CRT_ERROR, _CRTDBG_FILE_STDERR);
 #    endif
 #endif
 
