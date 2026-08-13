c60d6067f95212309bc9626528dd17121ab3ffa1
Connor Briggs <cgbriggs99@gmail.com>
2026-08-13 15:02:12 -0400

    Have the specific test run on its own for output

--- testing/testing/src/main.cpp
+++ testing/testing/src/main.cpp
@@ -37,6 +37,7 @@ extern "C" void __cdecl einsums_invalid_parameter(wchar_t const *const expressio
     } else {
         std::fputs("Error at unknown location!", stderr);
     }
+    std::flush(stderr);
 }
 #endif
 
@@ -70,12 +71,16 @@ int main(int argc, char **argv) {
     auto prev_handler = _set_invalid_parameter_handler(einsums_invalid_parameter);
 
 #    ifdef EINSUMS_DEBUG
+    std::printf("Setting report modes.");
+    std::flush(stdout);
     _CrtSetReportMode(_CRT_ASSERT, _CRTDBG_MODE_FILE);
-//    _CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_FILE);
-//    _CrtSetReportMode(_CRT_ERROR, _CRTDBG_MODE_FILE);
+    //    _CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_FILE);
+    //    _CrtSetReportMode(_CRT_ERROR, _CRTDBG_MODE_FILE);
     _CrtSetReportFile(_CRT_ASSERT, _CRTDBG_FILE_STDERR);
-//    _CrtSetReportFile(_CRT_WARN, _CRTDBG_FILE_STDERR);
-//    _CrtSetReportFile(_CRT_ERROR, _CRTDBG_FILE_STDERR);
+    //    _CrtSetReportFile(_CRT_WARN, _CRTDBG_FILE_STDERR);
+    //    _CrtSetReportFile(_CRT_ERROR, _CRTDBG_FILE_STDERR);
+    std::printf("Finished setting report modes.");
+    std::flush(stdout);
 #    endif
 #endif
 
