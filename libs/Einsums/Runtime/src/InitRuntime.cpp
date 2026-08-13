ee04993a1462672a329a16d006adeb9c727051b0
Connor Briggs <cgbriggs99@gmail.com>
2026-08-13 19:53:35 -0400

    Try to get python to work

--- libs/Einsums/Runtime/src/InitRuntime.cpp
+++ libs/Einsums/Runtime/src/InitRuntime.cpp
@@ -107,7 +107,10 @@ int run(std::function<int()> const &f, std::vector<std::string> const &argv, Ini
         auto version_str = build_string();
         EINSUMS_LOG_INFO("Starting Einsums: {}", version_str);
 
-        EINSUMS_LOG_TRACE("Version string's data is at {}.", static_cast<void const *>(version_str.c_str()));
+        EINSUMS_LOG_TRACE("Version string's data is at {}. Trying to clear it.", static_cast<void const *>(version_str.c_str()));
+
+        version_str.clear();
+        EINSUMS_LOG_TRACE("Version string cleared. Trying to delete it.");
     }
 
     if (global_config.get_bool("install-signal-handlers", false)) {
