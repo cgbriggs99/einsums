c74e3b8448802020a599467e2ef6878c72f9d1af
Connor Briggs <cgbriggs99@gmail.com>
2026-08-13 17:43:41 -0400

    Change where conditional is in the job spec.

--- libs/Einsums/Runtime/src/InitRuntime.cpp
+++ libs/Einsums/Runtime/src/InitRuntime.cpp
@@ -102,8 +102,8 @@ int run(std::function<int()> const &f, std::vector<std::string> const &argv, Ini
     // Report build settings.
     EINSUMS_LOG_INFO("Starting Einsums: {}", build_string());
 
-    EINSUMS_LOG_TRACE("The global configuration is at {}.", static_cast<void const *>(std::addressof(global_config)));
-    EINSUMS_LOG_TRACE("The global bool map is at {}.", static_cast<void const *>(global_config.get_bool_map().get()));
+    EINSUMS_LOG_INFO("The global configuration is at {}.", static_cast<void const *>(std::addressof(global_config)));
+    EINSUMS_LOG_INFO("The global bool map is at {}.", static_cast<void const *>(global_config.get_bool_map().get()));
 
     if (global_config.get_bool("install-signal-handlers", false)) {
         EINSUMS_LOG_INFO("Installing signal handlers...");
