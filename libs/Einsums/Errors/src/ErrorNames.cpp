9e190d2c1584eda1c3b7c99f8f4e44e77171481a
Connor Briggs <cgbriggs99@gmail.com>
2026-08-14 14:15:20 -0400

    Trying to find where the next acess violation is.

--- libs/Einsums/Errors/src/ErrorNames.cpp
+++ libs/Einsums/Errors/src/ErrorNames.cpp
@@ -5,13 +5,21 @@
 
 #include <Einsums/Errors/Error.hpp>
 
+#include <cstdio>
+#include <ostream>
 #include <fmt/format.h>
 
 namespace einsums::detail {
 
 std::string make_error_message(std::string_view const &type_name, char const *str, std::source_location const &location) {
-    return fmt::format("{}:{}:{}:\nIn {}\n{}: {}", location.file_name(), location.line(), location.column(), location.function_name(),
-                       type_name, str);
+    std::ostringstream out;
+    
+    out << location.file_name() << ":" << location.line() << ":" << location.column() << ":" << std::endl;
+    out << "In " << location.function_name() << std::endl;
+    out << str;
+    return out.str();
+//    return fmt::format("{}:{}:{}:\nIn {}\n{}: {}", location.file_name(), location.line(), location.column(), location.function_name(),
+//                       type_name, str);
 }
 
 std::string make_error_message(std::string_view const &type_name, std::string const &str, std::source_location const &location) {
