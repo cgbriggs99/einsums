170b82126df7b79629ac1eae832cc6d8c1550757
Connor Briggs <cgbriggs99@gmail.com>
2026-08-18 17:17:28 -0400

    Add some trace statements.

--- libs/Einsums/Assertion/src/Assertion.cpp
+++ libs/Einsums/Assertion/src/Assertion.cpp
@@ -10,12 +10,24 @@
 #include <Einsums/Version.hpp>
 
 #include <iostream>
+#include <mutex>
+
+namespace {
+einsums::detail::assertion_handler_type handler{einsums::detail::default_assertion_handler};
+
+std::mutex handler_mutex;
+} // namespace
 
 namespace einsums::detail {
 
 namespace {
 auto get_handler() -> assertion_handler_type & {
-    static assertion_handler_type handler{default_assertion_handler};
+    std::lock_guard lock(handler_mutex);
+
+    if (handler == nullptr) {
+        handler = default_assertion_handler;
+    }
+
     return handler;
 }
 } // namespace
@@ -33,15 +45,40 @@ void default_assertion_handler(std::source_location const &loc, char const *expr
     std::exit(EXIT_FAILURE);
 }
 
-void set_assertion_handler(assertion_handler_type handler) {
-    get_handler() = handler;
+void set_assertion_handler(assertion_handler_type handler_) {
+    std::lock_guard lock(handler_mutex);
+
+    EINSUMS_LOG_DEBUG("Setting assertion handler.");
+
+    if (handler_ == nullptr) {
+        EINSUMS_LOG_TRACE("Got a null pointer for the handler. Setting it back to the default instead.");
+        handler = default_assertion_handler;
+    } else {
+        EINSUMS_LOG_TRACE("Got an actual handler. Setting.");
+        handler = handler_;
+    }
 }
 
 void handle_assert(std::source_location const &loc, char const *expr, std::string const &msg) noexcept {
-    if (get_handler() == nullptr) {
-        default_assertion_handler(loc, expr, msg);
+    std::lock_guard lock(handler_mutex);
+    EINSUMS_LOG_DEBUG(complete_version());
+    EINSUMS_LOG_DEBUG("{}: {}: Assertion '{}' failed", loc.function_name(), loc.line(), expr);
+
+#ifdef EINSUMS_DEBUG
+    if (!msg.empty()) {
+        EINSUMS_LOG_DEBUG(msg);
     }
-    get_handler()(loc, expr, msg);
+#endif
+
+    if (handler == nullptr) {
+        handler = default_assertion_handler;
+    }
+
+    EINSUMS_LOG_TRACE("Delegating to the assertion handler.");
+
+    handler(loc, expr, msg);
+
+    EINSUMS_LOG_TRACE("Finished delegating to the assertion handler.");
 }
 
 } // namespace einsums::detail
